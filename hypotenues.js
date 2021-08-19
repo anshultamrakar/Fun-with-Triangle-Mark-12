bd9432e1abf06569f3fdf8c400bb0f4804103466
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-19 15:02:29 -0400

    Update Hypoteneus.js

--- hypotenues.js
+++ hypotenues.js
@@ -5,15 +5,15 @@ const outputEl = document.querySelector("#output");
 
  function calculateHypo(a,b){
     const sumOfsqtr = a * a + b * b  ;
-    console.log(sumOfsqtr)
+    return sumOfsqtr ;
  }
 
 
 
 function calculateHypoteneus(){
-     const sumOfsqrt = calculateHypo(Number(hypoInputs[0].value), Number(hypoInputs[1].value), Number(hypoInputs[2].value));
-               const c = Math.sqrt(sumOfsqrt);
-               console.log(c);
+        const sumOfsqrt = calculateHypo(Number(hypoInputs[0].value), Number(hypoInputs[1].value), Number(hypoInputs[2].value));
+        const lengthOfHypo = Math.sqrt(sumOfsqrt);
+        console.log(lengthOfHypo);
 }
 
 hypoButton.addEventListener("click", calculateHypoteneus);
