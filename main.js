edec3881ebcb921a094112b0bd034a1c5b2aae9d
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-23 12:16:34 -0400

    bug fix

--- main.js
+++ main.js
@@ -4,19 +4,17 @@ const outputEl = document.querySelector("#output");
 
 function calculateSumOfAngles(angle1, angle2, angle3){
     const sumOfAngles =  angle1 +  angle2  +  angle3
-    console.log(sumOfAngles );
+    console.log(sumOfAngles);
 }
 
 
 function isTriangle(){
     const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
-    
-    if(sumOfAngles === 180 ){
-      sumOfAngles.innerText = "It is a triangle !"
-      
-    } else {
-        sumOfAngles.innerText = "It is not a triangle !"
-    }
+ if(sumOfAngles === 180){
+    outputEl.innerText = "Its a triangle !"
+ }else{
+    outputEl.innerText = "Its not a triangle !"
+ }
  
 }
 
