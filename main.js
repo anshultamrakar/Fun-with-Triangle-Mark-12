dff265b5e10e81b4b104f03d076d3916a1f0382a
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-23 21:42:25 -0400

    done with project
    Still having some bugs

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const isTriangleBtn = document.querySelector("#btn");
 const outputEl = document.querySelector("#output");
 
 function calculateSumOfAngles(angle1, angle2, angle3){
-    const sumOfAngles =  angle1 +  angle2  +  angle3
+ const sumOfAngles =  angle1 +  angle2  +  angle3
     console.log(sumOfAngles);
 }
 
 
 function isTriangle(){
-    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
+  const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
  if(sumOfAngles === 180){
-    outputEl.innerText = "Its a triangle !"
+    outputEl.innerText = "It is a triangle "
  }else{
-    outputEl.innerText = "Its not a triangle !"
+    outputEl.innerText = "It is  not a triangle "
  }
- 
+
 }
 
 isTriangleBtn.addEventListener("click", isTriangle);
--- quiz.js
+++ quiz.js
@@ -3,21 +3,21 @@ const submitButton = document.querySelector("#submit-btn");
 const outputEl = document.querySelector("#output");
 
 
-const correctAnswer = ["90", "right angled "];
+const correctAnswer = ["90","right angled"];
 
 function calculateScore(){
     let score = 0 ;
     let index = 0 ;
     const formResults = new FormData(quizForm);
-    for (let value of formResults.values()){
+    for (let value of formResults.value()){
       
         if(value === correctAnswer[index]){
             score = score + 1;
         }
-        index = index + 1 ;
+        index = index + 1;
     }
    
-    outputEl.innerText = "Your score is " + score ;
+    outputEl.innerText = "Your score is " + score;
 }
 
 
